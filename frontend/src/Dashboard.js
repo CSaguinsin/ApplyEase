91e70d24e73cc39c0de0612ef680e33a8ace8ac5
Kevin Yu <kevinyu2201@gmail.com>
2024-10-24 23:01:31 +0800

    Dashboard development ongoing

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -3,20 +3,41 @@ import { HiOutlineBriefcase } from "react-icons/hi";
 import { CiGlobe } from "react-icons/ci";
 import { BiTask } from "react-icons/bi";
 import { LuPhone } from "react-icons/lu";
+import { IoPerson } from "react-icons/io5";
+import { IoMdSettings } from "react-icons/io";
+import { IoIosNotifications } from "react-icons/io";
+
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
     return (
         <>
             <div className="absolute top-0 left-0 w-full h-64 bg-blue-500">
                 <div className="flex">
-                    <Navigation page="Dashboard" />
-                    <div className="mt-8 ml-8 text-white">
-                        Pages / Dashboard
-                        <div className="font-bold">
-                            Dashboard
+                    <Navigation page="Candidate" />
+                    <div className="w-screen m-8">
+                        <div className="flex items-center justify-between">
+                            <div className="text-white">
+                                Pages / Dashboard
+                                <div className="font-bold">
+                                    Dashboard
+                                </div>
+                            </div>
+                            <div className="flex items-center font-bold text-white">
+                                <Link to="#" className="flex items-center m-3">
+                                    <IoPerson className="text-white m-1" />
+                                    Akari
+                                </Link>
+                                <Link to="#" className="m-3">
+                                    <IoMdSettings className="text-white text-xl" />
+                                </Link>
+                                <Link to="#" className="m-3">
+                                    <IoIosNotifications className="text-white text-xl" />
+                                </Link>
+                            </div>
                         </div>
-                        <div className="mt-6 ml-6 mr-2 grid gap-6 grid-cols-4">
-                            <div className="bg-white rounded text-blue-400 h-16 w-64 rounded-xl shadow-md p-3 flex items-center justify-center">
+                        <div className="m-6 w-11/12 grid gap-4 grid-cols-4">
+                            <div className="bg-white rounded text-blue-400 h-16 rounded-xl shadow-lg p-3 flex items-center justify-between">
                                 <div className="flex-block">
                                     <div className="text-xs">
                                         Applications Overview
@@ -26,11 +47,11 @@ export default function Dashboard() {
                                         <span className="text-green-600 text-xs">+20%</span>
                                     </div>
                                 </div>
-                                <div className="bg-blue-400 p-2 rounded-md ml-16 mr-0">
+                                <div className="bg-blue-400 p-2 rounded-md">
                                     <HiOutlineBriefcase className="text-3xl text-white" />
                                 </div>
                             </div>
-                            <div className="bg-white rounded text-blue-400 h-16 w-64 rounded-xl shadow-md p-3 flex items-center justify-center">
+                            <div className="bg-white rounded text-blue-400 h-16 rounded-xl shadow-lg p-3 flex items-center justify-between">
                                 <div className="flex-block">
                                     <div className="text-xs">
                                         Job Search
@@ -40,11 +61,11 @@ export default function Dashboard() {
                                         <span className="text-red-600 text-xs">-5%</span>
                                     </div>
                                 </div>
-                                <div className="bg-blue-400 p-2 rounded-md ml-32 mr-0">
+                                <div className="bg-blue-400 p-2 rounded-md">
                                     <CiGlobe className="text-3xl text-white" />
                                 </div>
                             </div>
-                            <div className="bg-white rounded text-blue-400 h-16 w-64 rounded-xl shadow-md p-3 flex items-center justify-center">
+                            <div className="bg-white rounded text-blue-400 h-16 rounded-xl shadow-lg p-3 flex items-center justify-between">
                                 <div className="flex-block">
                                     <div className="text-xs">
                                         Resume Management
@@ -54,11 +75,11 @@ export default function Dashboard() {
                                         <span className="text-green-600 text-xs">+30%</span>
                                     </div>
                                 </div>
-                                <div className="bg-blue-400 p-2 rounded-md ml-16 mr-0">
+                                <div className="bg-blue-400 p-2 rounded-md">
                                     <BiTask className="text-3xl text-white" />
                                 </div>
                             </div>
-                            <div className="bg-white rounded text-blue-400 h-16 w-64 rounded-xl shadow-md p-3 flex items-center justify-center">
+                            <div className="bg-white rounded text-blue-400 h-16 rounded-xl shadow-lg p-3 flex items-center justify-between">
                                 <div className="flex-block">
                                     <div className="text-xs">
                                         Job Offers
@@ -68,9 +89,60 @@ export default function Dashboard() {
                                         <span className="text-green-600 text-xs">+10%</span>
                                     </div>
                                 </div>
-                                <div className="bg-blue-400 p-2 rounded-md ml-32 mr-0">
+                                <div className="bg-blue-400 p-2 rounded-md">
                                     <LuPhone className="text-3xl text-white" />
                                 </div>
+                            </div>
+                        </div>
+                        <div className="m-6 w-11/12 grid grid-cols-2 gap-4">
+                            <div className="bg-white rounded-xl h-40 shadow-md border p-2 flex">
+                                <div className="mr-6">
+                                    <div className="text-blue-400">
+                                        Built by developers
+                                    </div>
+                                    <div className="text-black text-3xl font-bold">
+                                        Craftora
+                                    </div>
+                                    <div className="text-blue-400 text-xs mb-6">
+                                        Empowering creativity through innovative tools and collaborative solutions for makers and visionaries.
+                                    </div>
+                                    <Link to="#" className="text-gray-700 text-xs">
+                                        Read more...
+                                    </Link>
+                                </div>
+                                <div>
+                                    <img src='./Image.png' className="object-cover w-96 h-full rounded-lg" />
+                                </div>
+                            </div>
+
+                            <div className="bg-white rounded-xl h-40 shadow-md border p-2 flex">
+                                <img src="./brooke-cagle-g1Kr4Ozfoac-unsplash 1.png" className="brightness-50 object-cover w-full h-full rounded-lg fill-gray-950" />
+                                <div className="absolute p-2 text-white">
+                                    <div className="font-bold text-lg">
+                                        Work with Global Leaders
+                                    </div>
+                                    <div className="text-xs w-96">
+                                        Mollit consequat voluptate excepteur amet pariatur qui. Qui quis nulla excepteur aliquip commodo. Ex labore incididunt Lorem duis dolore officia enim occaecat ex ea aliquip cillum sunt magna.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="m-6 w-11/12 bg-white rounded-xl shadow-md p-6">
+                            <div className="text-gray-600 font-bold text-4xl">
+                                Applicant's Table
+                            </div>
+                            <div className="flex w-full">
+                                <table className="table-fixed">
+                                    <thead>
+                                        <tr>
+                                            <td>CANDIDATE NAME</td>
+                                            <td>JOB TITLE</td>
+                                            <td>STATUS</td>
+                                            <td>APPLICATION DATE</td>
+                                            <td>ACTIONS</td>
+                                        </tr>
+                                    </thead>
+                                </table>
                             </div>
                         </div>
                     </div>
--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -9,7 +9,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navigation({ page }) {
     return (
-        <nav className="mt-8 ml-8 w-64 h-96 shadow-md flex justify-center items-center p-6 rounded-3xl bg-white">
+        <nav className="mt-8 ml-8 w-64 h-96 shadow-md flex justify-center p-6 rounded-3xl bg-white">
             <div>
                 <div className="border-0 border-b-2 px-5 mb-5">
                     <img src="./ApplyEase@2x.png" className="h-12" />
@@ -18,14 +18,14 @@ export default function Navigation({ page }) {
                     <ul>
                         <li>
                             {page == "Dashboard" ? (
-                                <Link to='#' className="py-2 flex items-center text-black text-sm text-bold">
-                                    <div className="bg-blue-400 mr-4">
+                                <Link to='#' className="py-2 px-2 flex items-center text-black text-sm font-black shadow">
+                                    <div className="bg-blue-400 mr-4 rounded p-1">
                                         <MdHome className="text-lg text-bold text-white" />
                                     </div>
                                     Home
                                 </Link>
                             ) : (
-                                <Link to='#' className="py-2 flex items-center text-blue-400 text-sm">
+                                <Link to='#' className="py-2 px-2 flex items-center text-blue-400 text-sm">
                                     <MdHome className="mr-4 text-xl text-bold" />
                                     Home
                                 </Link>
@@ -33,34 +33,37 @@ export default function Navigation({ page }) {
                             }
                         </li>
                         <li>
-                            <Link to='#' className="py-2 flex items-center text-blue-400 text-sm">
-                                <IoSearchSharp className="mr-4 text-xl text-bold" />
-                                Job Search
-                            </Link>
+                            {page == "Application" ? (
+                                <Link to='#' className="py-2 px-2 flex items-center text-black text-sm font-black shadow">
+                                    <div className="bg-blue-400 mr-4 rounded p-1">
+                                        <IoSearchSharp className="text-lg text-bold text-white" />
+                                    </div>
+                                    Application
+                                </Link>
+                            ) : (
+                                <Link to='#' className="py-2 px-2 flex items-center text-blue-400 text-sm">
+                                    <IoSearchSharp className="mr-4 text-xl text-bold" />
+                                    Application
+                                </Link>
+
+                            )
+
+                            }
                         </li>
                         <li>
-                            <Link to='#' className="py-2 flex items-center text-blue-400 text-sm">
-                                <TbFileMinus className="mr-4 text-xl text-bold" />
-                                My Applications
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='#' className="py-2 flex items-center text-blue-400 text-sm">
-                                <VscFiles className="mr-4 text-xl text-bold" />
-                                Resume Manager
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='#' className="py-2 flex items-center text-blue-400 text-sm">
-                                <FaRegCalendar className="mr-4 text-xl text-bold" />
-                                Interview Schedule
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='#' className="py-2 flex items-center text-blue-400 text-sm">
-                                <MdOutlineFolderCopy className="mr-4 text-xl text-bold" />
-                                Job Offers
-                            </Link>
+                            {page == "Candidate" ? (
+                                <Link to='#' className="py-2 px-2 flex items-center text-black text-sm font-black shadow">
+                                    <div className="bg-blue-400 mr-4 rounded p-1">
+                                        <MdOutlineFolderCopy className="text-lg text-bold text-white" />
+                                    </div>
+                                    Candidate
+                                </Link>
+                            ) : (
+                                <Link to='#' className="py-2 px-2 flex items-center text-blue-400 text-sm">
+                                    <MdOutlineFolderCopy className="mr-4 text-xl text-bold" />
+                                    Candidate
+                                </Link>
+                            )}
                         </li>
                     </ul>
                 </div>
