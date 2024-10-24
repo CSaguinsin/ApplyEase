232c46c016e3a9479b14a6a65c669c41c8b182da
Kevin Yu <kevinyu2201@gmail.com>
2024-10-24 14:17:04 +0800

    Second Commit - Frontend

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,38 +1,55 @@
 import React from 'react';
-import { CiMail } from "react-icons/ci";
-import { RiLockPasswordLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
+import SignUp from './SignUp.js';
 
 export default function Login() {
     return (
-        <div className="w-full h-screen flex-block justify-center items-center bg-green-200">
-            <div className="flex justify-center">
-                <img src='./ApplyEase@2x.png' className='h-28' />
+        <div className="w-full h-screen flex justify-center items-center bg-blue-500">
+            <div>
+
+                <div className="flex justify-center">
+                    <img src='./ApplyEase-White@2x.png' className='h-16 mb-6' />
+                </div>
+                <div className="max-w-sm flex justify-center items-center">
+                    <form className="bg-white p-6 rounded-2xl">
+                        <label className="text-sm">
+                            Email:
+                            <input
+                                type="text"
+                                placeholder="Your email"
+                                className="py-2 px-3 w-full border rounded text-green-900 focus:outline-none mt-2 mb-4"
+                            />
+                        </label>
+                        <label className="text-sm">
+                            Password:
+                            <input
+                                type="password"
+                                placeholder="**********"
+                                className="py-2 px-3 w-full border rounded text-green-900 focus:outline-none mt-2 mb-4"
+                            />
+                        </label>
+                        <label className="flex items-center mb-6">
+                            <input
+                                type="checkbox"
+                                className="sr-only peer"
+                            />
+                            <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                            <span className="ml-2 text-xs">
+                                Remember me
+                            </span>
+                        </label>
+                        <button className="w-full bg-blue-800 text-white p-2 text-xs rounded-md shadow focus:outline-none hover:bg-blue-700 hover:text-white">
+                            SIGN IN
+                        </button>
+                        <div className="mt-5 text-sm flex justify-center">
+                            Don't have an account?
+                        </div>
+                        <div className="text-sm flex justify-center text-blue-900 font-black">
+                            <Link to='/signup'>Sign Up</Link>
+                        </div>
+                    </form>
+                </div >
             </div>
-            <form className="bg-white rounded-md max-w-lg p-6" style={{ width: 800 }}>
-                <label>
-                    Email:
-                    <div className="mt-2 mb-6 w-full flex items-center border-0 border-b-2 px-3">
-                        <CiMail className="text-xl text-gray-900" />
-                        <input
-                            type="email"
-                            className="py-2 px-3 w-full text-green-900 focus:outline-none"
-                        />
-                    </div>
-                </label>
-                <label>
-                    Password:
-                    <div className="mt-2 mb-6 w-full flex items-center">
-                        <RiLockPasswordLine className="text-xl text-gray-900" />
-                        <input
-                            type="password"
-                        />
-                    </div>
-                </label>
-                <button>
-                    Login
-                </button>
-            </form>
         </div>
 
     )
--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import { CiUser, CiMail } from "react-icons/ci";
-import { RiLockPasswordLine, RiLockPasswordFill } from "react-icons/ri";
 import { Link } from 'react-router-dom';
 
 export default function SignUp() {
     return (
-        <div className="bg-blue-700 flex h-screen w-full justify-center items-center">
-
+        <div className="bg-blue-500 flex h-screen w-full justify-center items-center">
             <div>
                 <div className="w-full flex justify-center items-align text-2xl font-bold text-white mb-2 block">
                     Welcome!
@@ -14,59 +11,51 @@ export default function SignUp() {
                 <div className="w-full flex justify-center items-align text-sm font-thin text-white mb-6">
                     Unlock your job search potential-sign up for free today
                 </div>
-                <div className="w-full flex justify-center items-center">
-                    <form className="bg-white p-6 rounded-md" style={{ width: 500 }}>
+                <div className="max-w-sm flex justify-center items-center">
+                    <form className="bg-white p-6 rounded-2xl">
                         <label className="text-sm">
                             Name:
-                            <div className="mt-2 mb-6 w-full flex items-center border-0 border-b-2 inline-block px-3">
-                                <CiUser className="text-xl" />
-                                <input
-                                    type="text"
-                                    placeholder="Surname, Firstname, Middlename"
-                                    className="py-2 px-3 w-full text-green-900 focus:outline-none"
-                                />
-                            </div>
+                            <input
+                                type="text"
+                                placeholder="Your full name"
+                                className="py-2 px-3 w-full border rounded text-green-900 focus:outline-none mt-2 mb-4"
+                            />
                         </label>
                         <label className="text-sm">
                             Email:
-                            <div className="mt-2 mb-6 w-full flex items-center border-0 border-b-2 inline-block px-3">
-                                <CiMail className="text-xl text-gray-900" />
-                                <input
-                                    type="email"
-                                    placeholder="example@example.com"
-                                    className="py-2 px-3 w-full text-green-900 focus:outline-none"
-                                />
-                            </div>
+                            <input
+                                type="Your email address"
+                                placeholder="example@example.com"
+                                className="py-2 px-3 w-full border rounded text-green-900 focus:outline-none mt-2 mb-4"
+                            />
 
                         </label>
                         <label className="text-sm">
                             Password:
-                            <div className="mt-2 mb-6 w-full flex items-center border-0 border-b-2 inline-block px-3">
-                                <RiLockPasswordLine className="text-xl text-gray-900" />
-
-                                <input
-                                    type="password"
-                                    placeholder="**********"
-                                    className="py-2 px-3 w-full text-green-900 focus:outline-none"
-                                />
-                            </div>
+                            <input
+                                type="password"
+                                placeholder="**********"
+                                className="py-2 px-3 w-full border rounded text-green-900 focus:outline-none mt-2 mb-4"
+                            />
                         </label>
-                        <label className="text-sm">
-                            Confirm Password:
-                            <div className="mt-2 mb-6 w-full flex items-center border-0 border-b-2 inline-block px-3">
-                                <RiLockPasswordFill className="text-xl text-gray-900" />
-                                <input
-                                    type="password"
-                                    placeholder="**********"
-                                    className="py-2 px-3 w-full text-green-900 focus:outline-none"
-                                />
-                            </div>
+                        <label className="flex items-center mb-6">
+                            <input
+                                type="checkbox"
+                                className="sr-only peer"
+                            />
+                            <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-300 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                            <span className="ml-2 text-xs">
+                                Remember me
+                            </span>
                         </label>
-                        <button className="w-full bg-green-300 p-2 rounded-md shadow focus:outline-none hover:bg-green-700 hover:text-white">
-                            Sign Up
+                        <button className="w-full bg-blue-800 text-white p-2 text-xs rounded-md shadow focus:outline-none hover:bg-blue-700 hover:text-white">
+                            SIGN UP
                         </button>
-                        <div className="mt-5 text-sm italic hover:underline">
-                            <Link to='/login'>Already a member?</Link>
+                        <div className="mt-5 text-sm flex justify-center">
+                            Already have an account?
+                        </div>
+                        <div className="text-sm flex justify-center text-blue-900 font-black">
+                            <Link to='/login'>Sign In</Link>
                         </div>
                     </form>
                 </div >
